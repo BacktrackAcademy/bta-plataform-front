c0ec3b01514179a9cdc3fc644ed0f591ae57369f
Jhose Portillo <jhosmar26.00@gmail.com>
2022-01-12 17:01:30 -0500

    ADD: burger menu and burguer button inserted in header

--- components/header.vue
+++ components/header.vue
@@ -1,12 +1,17 @@
 <template>
   <nav class="top-0">
     <div
-      class="fixed w-60 bg-red-600 top-0 left-0 z-0 h[100px] translate-y-52"
+      :class="{ ' -translate-y-[300%]': !hamburgerMenuIsOpen}"
+      class="block lg:hidden fixed w-full bg-body-back top-20 left-0 z-0 overflow-y-scroll h-full max-h-[calc(100%-5rem)] duration-500"
     >
-      <HamburgerMenu />
+      <HamburgerMenu :close="closeBurgerMenu" />
     </div>
-    <div class="fixed flex gap-6 items-center bg-black px-8 lg:px-16 py-4 w-full z-10">
-      <img class="w-32" src="~/assets/logo.svg" />
+    <div class="fixed top-0 left-0 flex gap-6 items-center bg-header-back px-8 lg:px-16 w-full h-20 z-10">
+      <div @click.prevent="closeBurgerMenu">
+        <nuxt-link to="/">
+          <img class="w-32" src="~/assets/logo.svg" />
+        </nuxt-link>
+      </div>
       <div class="hidden lg:block relative">
         <form class="flex flex-row-reverse gap-1 bg-gray-border items-center px-3 py-2 rounded-full overflow-hidden" action="/search">
           <input
@@ -37,20 +42,31 @@
         <div class="text-white font-oswald border-principal border px-3 py-1 flex-shrink-0">Vuélvete Pro!</div>
         <div class="text-white font-oswald">Iniciar Sesión</div>
       </div>
-      <HamburgerButton/>
+      <HamburgerButton :toggleProp="toggleBurgerMenu" :state="hamburgerMenuIsOpen" />
     </div>
   </nav>
 </template>
 
 <script>
-import HamburgerButton from "~/components/hamburgerMenu/hamburgerButton.vue"
+  import HamburgerButton from "~/components/hamburgerMenu/hamburgerButton.vue"
+
   export default {
     name: "header",
     components: {
       HamburgerButton
     },
     data() {
-      return {}
+      return {
+        hamburgerMenuIsOpen: false,
+      }
+    },
+    methods: {
+      closeBurgerMenu() {
+        this.hamburgerMenuIsOpen = false;
+      },
+      toggleBurgerMenu() {
+        this.hamburgerMenuIsOpen = !this.hamburgerMenuIsOpen;
+      },
     }
   }
 </script>
