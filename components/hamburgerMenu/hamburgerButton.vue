6a7e4d4d9ffca9b55209d40aad75f189756d305f
Jhose Portillo <jhosmar26.00@gmail.com>
2022-01-12 17:00:24 -0500

    ADD: burger button action

--- components/hamburgerMenu/hamburgerButton.vue
+++ components/hamburgerMenu/hamburgerButton.vue
@@ -2,12 +2,12 @@
   <div class="block lg:hidden text-white ml-auto">
     <div class="flex flex-col justify-center">
       <div class="relative sm:max-w-xl mx-auto">
-        <button class="text-white w-10 h-10 relative focus:outline-none" @click="open = !open">
+        <button class="text-white w-10 h-10 relative focus:outline-none" @click="toggle">
           <span class="sr-only">Open main menu</span>
           <div class="block w-5 absolute top-1/2 transform ">
-            <span aria-hidden="true" class="block absolute h-1.5 w-12 bg-current transform transition duration-500 ease-in-out rounded-full" :class="{'rotate-45 -bottom-1 translate-y-0': open,' -translate-y-1.5 -top-1.5': !open }"></span>
-            <span aria-hidden="true" class="block absolute h-1.5 w-12 bg-current transform transition duration-500 ease-in-out rounded-full" :class="{'opacity-0': open } "></span>
-            <span aria-hidden="true" class="block absolute h-1.5 w-12 bg-current transform transition-all duration-500 ease-in-out rounded-full" :class="{'-rotate-45 -bottom-1 translate-y-0': open, ' translate-y-1.5 -bottom-3': !open}"></span>
+            <span aria-hidden="true" class="block absolute h-1.5 w-12 bg-current transform transition duration-500 ease-in-out rounded-full" :class="{'rotate-45 -bottom-1 translate-y-0': this.state,' -translate-y-1.5 -top-1.5': !this.state }"></span>
+            <span aria-hidden="true" class="block absolute h-1.5 w-12 bg-current transform transition duration-500 ease-in-out rounded-full" :class="{'opacity-0': this.state } "></span>
+            <span aria-hidden="true" class="block absolute h-1.5 w-12 bg-current transform transition-all duration-500 ease-in-out rounded-full" :class="{'-rotate-45 -bottom-1 translate-y-0': this.state, ' translate-y-1.5 -bottom-3': !this.state}"></span>
           </div>
         </button>
       </div>
@@ -17,9 +17,15 @@
 <script>
   export default {
     name: "hamburgerButton",
+    props: ["toggleProp", "state"],
     data() {
       return {
-        open: false
+        // open: false
+      }
+    },
+    methods:{
+      toggle() {
+        this.toggleProp()
       }
     }
   }
