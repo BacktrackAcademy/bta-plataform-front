3d55b8927e121e29b644a5ddb742ef92b221b187
Jhose Portillo <jhosmar26.00@gmail.com>
2022-01-12 10:39:22 -0500

    EDIT: dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        principal: "#EC1075",
+        "gray-border": "#36364e",
+      },
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
