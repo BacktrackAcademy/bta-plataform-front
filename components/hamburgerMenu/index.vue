56707a64ecd590f22f08552c7cefba85d26704bf
CristopherGuzman <guzmncristopher@gmail.com>
2022-06-06 13:43:19 -0500

    FIX: menu colors

--- components/hamburgerMenu/index.vue
+++ components/hamburgerMenu/index.vue
@@ -2,7 +2,7 @@
   <div class="p-4 flex flex-col">
     <div class="relative rounded-lg overflow-hidden">
       <input type="text" class="w-full bg-[#242424] text-sm text-white rounded-lg px-4 py-2 placeholder:text-white placeholder:opacity-50" placeholder="Buscar área, especialidad, curso…">
-      <div class="w-14 h-full absolute top-0 right-0 bg-principal flex justify-center items-center">
+      <div class="w-14 h-full absolute top-0 right-0 bg-bta-pink flex justify-center items-center">
         <svg
             class="text-white searcher__icon"
             width="24"
@@ -30,7 +30,7 @@
       </div>
     </div>
     <div @click="closeFunction">
-      <nuxt-link to="/login" class="block bg-principal text-white text-xl font-oswald rounded-lg py-2 text-center my-0">Inicia sesión o Regístrate</nuxt-link>
+      <nuxt-link to="/login" class="block bg-bta-pink text-white text-xl font-oswald rounded-lg py-2 text-center my-0">Inicia sesión o Regístrate</nuxt-link>
     </div>
   </div>
 </template>
