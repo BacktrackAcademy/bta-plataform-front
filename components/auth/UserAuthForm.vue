719fa25b2ce0e24e6bd35e1014fed8f9d3573edc
CristopherGuzman <guzmncristopher@gmail.com>
2022-06-14 15:23:10 -0500

    ADD: styles social buttons

--- components/auth/UserAuthForm.vue
+++ components/auth/UserAuthForm.vue
@@ -5,7 +5,7 @@
       class="flex flex-col justify-center items-center rounded-lg gap-4 mobile:p-5 table:p-10"
       @submit.prevent="submitForm(userInfo)"
     >
-      <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+      <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4 w-full">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
         </svg>
@@ -18,7 +18,7 @@
         >
       </div>
 
-      <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+      <div class="flex items-center border-2 py-2 px-3 rounded-2xl w-full">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
         </svg>
