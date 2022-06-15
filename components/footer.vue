3580805ea5542c6063f6a654ec1c0e037cdb49d2
CristopherGuzman <guzmncristopher@gmail.com>
2022-06-15 17:56:40 -0500

    ADD: animation footer and fix spacing

--- components/footer.vue
+++ components/footer.vue
@@ -14,40 +14,51 @@
           </div>
         </div>
       </NuxtLink>
-      <div class="font-inconsolata text-left">
-        <h3 class="font-oswald mb-6 md:mt-6 text-2xl font-normal text-gray-200 uppercase">Recursos</h3>
+      <div class="font-inconsolata  text-lg md:text-left">
+        <h3 class="font-oswald mb-6  text-2xl font-normal text-gray-200 uppercase">Recursos</h3>
         <ul>
           <li class="mb-4"><NuxtLink to="/" 
-              class="text-gray-600 hover:underline">Empresas</NuxtLink></li>
+              class="foot__link">Empresas</NuxtLink></li>
           <li class="mb-4"><NuxtLink to="/" rel="nofollow"
-              class="text-gray-600 hover:underline">Usuarios</NuxtLink></li>
+              class="foot__link">Usuarios</NuxtLink></li>
           <li class="mb-4"><NuxtLink to="/" rel="nofollow"
-              class="text-gray-600 hover:underline">Nosotros</NuxtLink></li>
+              class="foot__link">Cursos Gratis</NuxtLink></li>
           <li class="mb-4"><NuxtLink to="/" rel="nofollow"
-              class="text-gray-600 hover:underline">Cursos Gratis</NuxtLink></li>
+              class="foot__link">Nosotros</NuxtLink></li>
           <li class="mb-4"><NuxtLink to="/" rel="nofollow"
-              class="text-gray-600 hover:underline">Patrocinios</NuxtLink></li>
+              class="foot__link">Patrocinios</NuxtLink></li>
           <li><NuxtLink to="/" rel="nofollow"
-              class="text-gray-600 hover:underline">Catálogo de Cursos</NuxtLink></li>
+              class="foot__link ">Catálogo de Cursos</NuxtLink></li>
         </ul>
       </div>
-      <div class="font-inconsolata text-left">
-        <h3 class="font-oswald mb-6 md:mt-6 text-2xl font-normal text-gray-200 uppercase ">Síguenos</h3>
+      <div class="font-inconsolata text-lg md:text-left">
+        <h3 class="font-oswald mb-6  text-2xl font-normal text-gray-200 uppercase ">Legal</h3>
         <ul>
-          <li class="mb-4"><a href="https://www.facebook.com/CymaSuite" target="_blank"
-              class="text-gray-600 hover:underline">Facebook</a></li>
-          <li class="mb-4"><a href="https://www.instagram.com/cymasuite/" target="_blank"
-              class="text-gray-600 hover:underline">Instagram</a></li>
-          <li><a href="https://www.linkedin.com/company/cymasuite/" target="_blank"
-              class="text-gray-600 hover:underline">LinkedIn</a></li>
-        </ul>
-      </div>
-      <div class="font-inconsolata text-left">
-        <h3 class="font-oswald mb-6 md:mt-6 text-2xl font-normal text-gray-200 uppercase ">Legal</h3>
-        <ul>
-          <li class="mb-4"><NuxtLink to="/"  class="text-gray-600 hover:underline">Políticas de Privacidad</NuxtLink>
+          <li class="mb-4"><NuxtLink to="/"  class="foot__link">Políticas de Privacidad</NuxtLink>
           </li>
-          <li><NuxtLink to="/" class="text-gray-600 hover:underline">Términos de servicio</NuxtLink></li>
+          <li class="mb-8"><NuxtLink to="/" class="foot__link">Términos de servicio</NuxtLink></li>
+        </ul>
+        <ul>
+          <li class="mb-4"><NuxtLink to="/" class="foot__link">Ranking</NuxtLink></li>
+          <li class="mb-4"><NuxtLink to="/"  class="foot__link">Preguntas Frecuentes</NuxtLink>
+          </li>
+          <li><NuxtLink to="/" class="foot__link">Valida tus certificado</NuxtLink></li>
+        </ul>
+      </div>
+      <div class="font-inconsolata text-lg md:text-left">
+        <h3 class="font-oswald mb-6  text-2xl font-normal text-gray-200 uppercase ">Hablemos</h3>
+        <ul>
+          <li class="mb-4"><a href="mailto:contacto@backtrackacademy.com" class="foot__link flex items-center gap-2">
+            <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="13.5" viewBox="0 0 18 13.5">
+                <path id="envelope" d="M17.659,68.458a.212.212,0,0,1,.341.165v7.189A1.688,1.688,0,0,1,16.313,77.5H1.687A1.688,1.688,0,0,1,0,75.813V68.627a.21.21,0,0,1,.341-.165c.787.612,1.832,1.389,5.418,3.994C6.5,73,7.752,74.136,9,74.129c1.255.011,2.531-1.153,3.245-1.673C15.831,69.85,16.871,69.07,17.659,68.458ZM9,73c.816.014,1.99-1.027,2.58-1.455,4.665-3.386,5.02-3.681,6.1-4.525A.841.841,0,0,0,18,66.355v-.668A1.688,1.688,0,0,0,16.313,64H1.687A1.688,1.688,0,0,0,0,65.688v.668a.846.846,0,0,0,.323.664c1.076.84,1.431,1.139,6.1,4.525C7.01,71.973,8.184,73.014,9,73Z" transform="translate(0 -64)" fill="#ec1075"></path>
+              </svg>
+            </span>
+            <span>contacto@backtrackacademy.com</span></a></li>
+          <li><a href="mailto:ventas@backtrackacademy.com" class="foot__link flex items-center gap-2">
+            <span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="13.5" viewBox="0 0 18 13.5">
+                <path id="envelope" d="M17.659,68.458a.212.212,0,0,1,.341.165v7.189A1.688,1.688,0,0,1,16.313,77.5H1.687A1.688,1.688,0,0,1,0,75.813V68.627a.21.21,0,0,1,.341-.165c.787.612,1.832,1.389,5.418,3.994C6.5,73,7.752,74.136,9,74.129c1.255.011,2.531-1.153,3.245-1.673C15.831,69.85,16.871,69.07,17.659,68.458ZM9,73c.816.014,1.99-1.027,2.58-1.455,4.665-3.386,5.02-3.681,6.1-4.525A.841.841,0,0,0,18,66.355v-.668A1.688,1.688,0,0,0,16.313,64H1.687A1.688,1.688,0,0,0,0,65.688v.668a.846.846,0,0,0,.323.664c1.076.84,1.431,1.139,6.1,4.525C7.01,71.973,8.184,73.014,9,73Z" transform="translate(0 -64)" fill="#ec1075"></path>
+              </svg>
+            </span>ventas@backtrackacademy.com</a></li>
         </ul>
       </div>
       <!-- <div>
@@ -62,19 +73,27 @@
     </div>
     <hr class=" border-bta-section sm:mx-auto ">
     <div class=" flex flex-col items-center justify-around">
-      <div class="flex  my-4 space-x-6 sm:justify-center "><a href="https://www.facebook.com/CymaSuite" target="_blank" class="text-gray-500 hover:text-white"><svg
+      <div class="mt-2 md:mt-0">
+        <h3 class="text-gray-400 font-oswald uppercase">Síguenos</h3>
+      </div>
+      <div class="flex  my-4 space-x-6 sm:justify-center "><a href="https://www.facebook.com/CymaSuite" target="_blank" class="text-gray-500 hover:text-bta-pink"><svg
             class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
             <path fill-rule="evenodd"
               d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
               clip-rule="evenodd"></path>
-          </svg></a><a href="https://www.instagram.com/cymasuite/" target="_blank" class="text-gray-500 hover:text-white"><svg class="w-5 h-5" fill="currentColor"
+          </svg></a><a href="https://www.instagram.com/cymasuite/" target="_blank" class="text-gray-500 hover:text-bta-pink"><svg class="w-5 h-5" fill="currentColor"
             viewBox="0 0 24 24" aria-hidden="true">
             <path fill-rule="evenodd"
               d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
               clip-rule="evenodd"></path>
-          </svg></a><a href="https://www.linkedin.com/company/cymasuite/" target="_blank" class="text-gray-500 hover:text-white"><svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet"><g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="currentColor" stroke="none"><path d="M2325 4970 c-478 -53 -889 -215 -1272 -501 -125 -94 -366 -335 -462 -462 -258 -345 -418 -723 -483 -1142 -30 -199 -30 -521 0 -720 180 -1166 1147 -2050 2312 -2114 268 -14 534 12 786 78 450 118 894 393 1200 742 333 379 529 798 606 1294 30 198 30 520 0 720 -84 540 -322 1014 -702 1395 -375 376 -838 608 -1387 695 -116 19 -483 28 -598 15z m-558 -810 c75 -32 147 -111 168 -185 32 -108 1 -211 -86 -289 -64 -58 -130 -80 -218 -74 -152 11 -249 112 -259 269 -4 67 -1 83 23 138 32 71 94 131 158 155 56 21 148 15 214 -14z m183 -1775 l0 -1085 -315 0 -315 0 0 1085 0 1085 315 0 315 0 0 -1085z m930 985 l0 -99 38 25 c54 38 177 98 252 123 98 34 201 51 309 51 176 0 304 -53 418 -174 76 -80 120 -154 157 -269 53 -161 56 -223 56 -1006 l0 -721 -310 0 -310 0 0 713 c0 805 2 786 -70 851 -22 20 -41 36 -43 36 -1 0 -29 4 -61 10 -167 27 -343 -57 -415 -198 -21 -41 -21 -51 -21 -727 l0 -685 -310 0 -310 0 0 1085 0 1085 310 0 310 0 0 -100z"/></g>
+          </svg></a><a href="https://www.linkedin.com/company/cymasuite/" target="_blank" class="text-gray-500 hover:text-bta-pink"><svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet"><g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="currentColor" stroke="none"><path d="M2325 4970 c-478 -53 -889 -215 -1272 -501 -125 -94 -366 -335 -462 -462 -258 -345 -418 -723 -483 -1142 -30 -199 -30 -521 0 -720 180 -1166 1147 -2050 2312 -2114 268 -14 534 12 786 78 450 118 894 393 1200 742 333 379 529 798 606 1294 30 198 30 520 0 720 -84 540 -322 1014 -702 1395 -375 376 -838 608 -1387 695 -116 19 -483 28 -598 15z m-558 -810 c75 -32 147 -111 168 -185 32 -108 1 -211 -86 -289 -64 -58 -130 -80 -218 -74 -152 11 -249 112 -259 269 -4 67 -1 83 23 138 32 71 94 131 158 155 56 21 148 15 214 -14z m183 -1775 l0 -1085 -315 0 -315 0 0 1085 0 1085 315 0 315 0 0 -1085z m930 985 l0 -99 38 25 c54 38 177 98 252 123 98 34 201 51 309 51 176 0 304 -53 418 -174 76 -80 120 -154 157 -269 53 -161 56 -223 56 -1006 l0 -721 -310 0 -310 0 0 713 c0 805 2 786 -70 851 -22 20 -41 36 -43 36 -1 0 -29 4 -61 10 -167 27 -343 -57 -415 -198 -21 -41 -21 -51 -21 -727 l0 -685 -310 0 -310 0 0 1085 0 1085 310 0 310 0 0 -100z"/></g>
           </svg></a></div>
-      <span class="text-sm text-gray-500 sm:text-center"><a href="#" class="">© 2022  Backtrack Academy. Todos los Derechos Reservados.</a></span>
+      <span class="text-sm text-gray-500 text-center"><a href="#" class="">© 2022  Backtrack Academy. · Av. Lib. O’Higgis #1302 70, Santiago, Chile. <br> Todos los Derechos Reservados.</a></span>
     </div>
   </footer>
 </template>
+<style>
+  .foot__link {
+    @apply text-gray-600 relative before:block before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-full before:bg-bta-pink before:scale-x-0 before:transition-all hover:before:scale-x-100;
+  }
+</style>
