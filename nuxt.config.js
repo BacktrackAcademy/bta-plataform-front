84688737a403a21903441bb3c1325043318f34bc
Gonzalo <gologdlr@gmail.com>
2022-06-22 13:31:02 -0500

    ADD: gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/vue-gtag'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
