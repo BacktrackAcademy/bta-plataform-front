66eb17d6515b274af1c3c47596177de3b6d832f7
Gonzalo <gologdlr@gmail.com>
2022-07-05 17:08:30 -0500

    ADD: endpoint opinion call

--- pages/curso/_slug/opiniones/index.vue
+++ pages/curso/_slug/opiniones/index.vue
@@ -22,55 +22,42 @@
       </ol>
     </nav>
     <section class="bg-bta-dark-blue pt-0">
-      <div class="flex flex-wrap max-w-[1200px] mx-auto gap-5 py-7 px-2 relative">
         <!-- comentarios -->
-        <article class="bg-bta-blue w-[calc(50%-1.5rem)] flex flex-col py-4 px-5 mb-6 rounded-xl">
-          <header class="flex mb-4">
-            <figure class="inline-block h-11 w-11 mr-4">
-              <img src="https://api.lorem.space/image/face?w=44&h=44&hash=ttym2me2" class="rounded-full" alt="avatar" />
-            </figure>
-            <div class="flex flex-col">
-              <h3 class="text-gray-muted font-normal">Gonzalo Gutierrez</h3>
-              <p class="font-medium mb-2 no-underline text-sky-400 inline-block ">@golitoSAC</p>
-              <div class="flex">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star"
-                  class="inline-block h-4 overflow-visible mr-2 fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 576 512">
-                  <path fill="#fddd5b"
-                    d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z">
-                  </path>
-                </svg>
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star"
-                  class="inline-block h-4 overflow-visible mr-2 fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 576 512">
-                  <path fill="#fddd5b"
-                    d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z">
-                  </path>
-                </svg>
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star"
-                  class="inline-block h-4 overflow-visible mr-2 fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 576 512">
-                  <path fill="#fddd5b"
-                    d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z">
-                  </path>
-                </svg>
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star"
-                  class="inline-block h-4 overflow-visible mr-2 fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 576 512">
-                  <path fill="#fddd5b"
-                    d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z">
-                  </path>
-                </svg>
+        <div class="flex flex-wrap max-w-[1200px] mx-auto gap-5 py-7 px-2 relative" >
+          <article v-for="(opinion, i) in opinions" :key="opinion.i + i" class="bg-bta-blue w-[calc(50%-1.5rem)] flex flex-col py-4 px-5 mb-6 rounded-xl">
+            <header class="flex mb-4">
+              <figure class="inline-block h-11 w-11 mr-4">
+                <img :src="opinion.user.avatar_url_small" class="rounded-full" :alt="opinion.user.name" />
+              </figure>
+              <div class="flex flex-col">
+                <h3 class="text-gray-muted font-normal">{{opinion.user.name}}</h3>
+                <p class="font-medium mb-2 no-underline text-sky-400 inline-block ">{{opinion.user.username}}</p>
+                <div class="flex">
+                  <svg v-for="(star,i) in opinion.evaluation" :key="i" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star"
+                    class="inline-block h-4 overflow-visible mr-2 fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512">
+                    <path fill="#fddd5b"
+                      d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z">
+                    </path>
+                  </svg>
+                  <svg v-for="(star,i) in (5 - opinion.evaluation)" :key="i" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star"
+                    class="inline-block h-4 overflow-visible mr-2 fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512">
+                    <path fill="currentColor"
+                      d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z">
+                    </path>
+                  </svg>
+                </div>
+              </div>
+            </header>
+            <div class="flex">
+              <div class="ml-3">
+                <p class="text-white">{{opinion.opinion}}</p>
               </div>
             </div>
-          </header>
-          <div class="flex">
-            <div class="ml-3">
-              <p class="text-white">Buen contenido introductorio y muy buenas explicaciones.</p>
-            </div>
-          </div>
-        </article>
-        <article class="bg-bta-blue w-[calc(50%-1.5rem)] flex flex-col py-4 px-5 mb-6 rounded-xl">
+          </article>
+        </div>
+        <!-- <article class="bg-bta-blue w-[calc(50%-1.5rem)] flex flex-col py-4 px-5 mb-6 rounded-xl">
           <header class="flex mb-4">
             <figure class="inline-block h-11 w-11 mr-4">
               <img src="https://api.lorem.space/image/face?w=44&h=44&hash=rrlz2me2" class="rounded-full" alt="avatar" />
@@ -116,8 +103,7 @@
                 poco a poco</p>
             </div>
           </div>
-        </article>
-      </div>
+        </article> -->
     </section>
   </div>
 </template>
@@ -128,15 +114,25 @@ export default {
   data() {
     return {
       course: [],
+      opinions: [],
+
     }
   },
   created() {
-    this.getCourse();
+    // this.getCourse();
+    this.getOpinions();
   },
   methods: {
     getCourse() {
+      console.log(this.$route.params.slug)
       this.$axios.get(`api/v1/course/${this.$route.params.slug}`).then((response) => {
         this.course = response.data;
+      })
+    },
+    getOpinions() {
+      console.log(this.$route.params.slug)
+      this.$axios.get(`api/v1/course/${this.$route.params.slug}/opinions`).then((response) => {
+        this.opinions = response.data;
       })
     }
   }
