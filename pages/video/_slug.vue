44f0b646d98c7b0c08da1c2e6e8f79beb834387c
CristopherGuzman <guzmncristopher@gmail.com>
2022-07-05 15:19:34 -0500

    FIX: view video

--- pages/video/_slug.vue
+++ pages/video/_slug.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="bg-bta-dark-blue">
     <div class="flex lg:mx-10 mt-2">
-      <div class="lg:w-8/12">
+      <div class="lg:w-9/12">
         <!-- Video player -->
         <div
           class="relative top-0 max-h-[calc(100vh - 52px)] lg:min-w-[1024px] mx-auto"
         >
           <div style="padding: 56.25% 0 0 0; position: relative">
             <iframe
-              src="https://player.vimeo.com/video/150485199?h=0eb117b38a&title=0&byline=0&portrait=0&badge=0"
+              :src="'https://player.vimeo.com/video/'+ video.url +'?h=0eb117b38a&title=0&byline=0&portrait=0&badge=0'"
               class="absolute top-0 left-0 w-full h-full"
               frameborder="0"
               allow="autoplay; fullscreen; picture-in-picture"
@@ -29,34 +29,25 @@
               />
               <!-- header content -->
               <div class="flex flex-wrap">
-                <a href="" class="block w-full">
+                <nuxt-link :to="'/curso/' + course.slug" class="block w-full">
                   <h2 class="text-white font-normal text-base">
-                    Nombre del curso
+                    {{ course.titulo }}
                   </h2>
-                </a>
+                </nuxt-link>
                 <img
-                  src="https://api.lorem.space/image/face?w=30&h=30&hash=l6rom7bp"
+                  :src="teacher.avatar_url"
                   alt=""
                   class="w-6 h-6 rounded-full mr-1"
                 />
-                <p class="text-gray-muted">Thomas Rodriguez</p>
+                <p class="text-gray-muted">{{ teacher.name }} {{ teacher.lastname}}</p>
               </div>
             </div>
 
             <div>
               <button
-                class="flex items-center bg-bta-pink text-white px-3 py-2 w-[144px] rounded-[8px] mr-2"
+                class="flex items-center bg-bta-pink text-white hover:bg-bta-pink/90 px-3 py-2 w-[144px] rounded-[8px] mr-2"
               >
-                <svg
-                  aria-hidden="true" focusable="false" data-prefix="far" data-icon="arrow-to-right"
-                  class="inline-block text-inherit w-4 overflow-visible mr-3" role="img"
-                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M200.1 99.5l148.4 148c4.7 4.7 4.7 12.3 0 17l-148.4 148c-4.7 4.7-12.3 4.7-17 0l-19.6-19.6c-4.8-4.8-4.7-12.5.2-17.1l97.1-93.7H12c-6.6 0-12-5.4-12-12v-28c0-6.6 5.4-12 12-12h248.8l-97.1-93.7c-4.8-4.7-4.9-12.4-.2-17.1l19.6-19.6c4.7-4.9 12.3-4.9 17-.2zM396 76v360c0 6.6 5.4 12 12 12h28c6.6 0 12-5.4 12-12V76c0-6.6-5.4-12-12-12h-28c-6.6 0-12 5.4-12 12z"
-                  ></path>
-                </svg>
+                <ArrowToRight class="mr-3" />
                 <span
                   class="uppercase text-left text-sm font-bold w-[137px] text-ellipsis whitespace-nowrap overflow-hidden"
                   >Probando el router</span
@@ -69,7 +60,7 @@
             <!-- header class -->
             <div class="flex items-center">
               <h1 class="text-2xl text-white font-bold leading-9 my-4">
-                01- Presentacion threat Hunting
+                {{ video.titlevideo }}
               </h1>
               <span class="text-[#cacaca] ml-3">1/20</span>
             </div>
@@ -86,7 +77,7 @@
         </div>
       </div>
 
-      <div class="lg:w-4/12 p-4">
+      <div class="lg:w-3/12 p-4">
         <!-- badge -->
         <div class="text-[#cacaca] text-center mb-5">
           <p class="font-inconsolata">Has estudiado</p>
@@ -102,12 +93,15 @@
           <h4 class="text-2xl text-white font-oswald font-medium mt-5 mb-3">
             Temario
           </h4>
-          <div v-for="video in videos" :key="video.id">
-            <a :href="'/video/' + video.slug" class="block w-full py-4">
-              <p class="text-white font-normal text-base">
-                {{ video.titlevideo }}
-              </p>
-            </a>
+          <div v-for="(theme, i) in syllabus" :key="theme.slug + i">
+            <h3>{{ theme.titulo }}</h3>
+            <div v-for="(video, i) in theme.videos" :key="video.slug + i">
+              <a :href="'/video/' + video.slug" class="block w-full py-4">
+                <p class="text-white font-normal text-base">
+                  {{ video.titlevideo }}
+                </p>
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -117,7 +111,11 @@
 </template>
 
 <script>
+import ArrowToRight from '../../components/icons/arrow-to-right.vue';
 export default {
+  components: {
+    ArrowToRight
+  },
   data() {
     return {
       course: {},
@@ -126,12 +124,12 @@ export default {
     };
   },
   async asyncData({ $axios, params }) {
-    console.log(params);
-    const { data } = await $axios.get("/api/v1/course/" + params.slug);
+    const { data } = await $axios.get("/api/v1/video/" + params.slug)
     return {
-      course: data,
-      teacher: data.teacher,
-      syllabus: data.syllabus,
+      video: data,
+      course: data.course,
+      teacher: data.course.teacher,
+      syllabus: data.course.syllabus,
     };
   },
   head() {
@@ -176,17 +174,5 @@ export default {
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     };
   },
-
-  // methods: {
-  //   getCourse() {
-  //     this.$axios
-  //       .get(`api/v1/course/${this.$route.params.slug}`)
-  //       .then((response) => {
-  //         this.course = response.data;
-  //         this.syllabus = response.data.syllabus;
-  //         this.videos = response.data.syllabus[0].videos;
-  //       });
-  //   },
-  // },
 };
 </script>
