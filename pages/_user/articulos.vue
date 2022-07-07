9a1cc429753cdeb7246ad0958188ddd0501ee480
CristopherGuzman <guzmncristopher@gmail.com>
2022-07-07 18:06:18 -0500

    ADD: user child - articles

---
+++ pages/_user/articulos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p class="text-white">Escribe artículos para la comunidad</p>
+    <div>
+      <p class="text-white">¡Comparte tu experiencia en Hacking Ético!</p>
+      <div>
+        <p class="text-white">
+          Buscamos personas del mundo de la seguridad motivadas, proactivas, que les guste escribir y quieran compartir su conocimiento con la comunidad.
+          Aún no publicas artículos con la comunidad, conoce los requisitos acá.
+        </p>
+        <div>
+          <button class="text-white bg-bta-pink px-4 py-2 font-bold font-oswald">
+            Crear nuevo artículo
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
