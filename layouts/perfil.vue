1dcdd29968c820153d151f9c50be3789b3fb5dbb
Cristopher Guzmán <guzmncristopher@gmail.com>
2023-02-27 09:43:59 -0500

    ADD: layout profile

---
+++ layouts/perfil.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="$auth.loggedIn">
+    <Navbarloged/>
+    <NavbarPerfil class="pt-20" />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+<script >
+import Header from "../components/header.vue";
+import Footer from "../components/footer.vue";
+import Navbarloged from "../components/Navbarloged.vue";
+import NavbarPerfil from "../components/NavbarPerfil.vue";
+export default {
+  components: {
+    Header,
+    Footer,
+    Navbarloged,
+    NavbarPerfil,
+  },
+};
+</script>
