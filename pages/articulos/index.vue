1816f659112827326a07677c22077de4aee0f836
Cristopher Guzmán <guzmncristopher@gmail.com>
2023-02-28 11:09:54 -0500

    ADD: articles page

---
+++ pages/articulos/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <main class="bg-bta-dark-blue px-4 sm:px-6 lg:px-8">
+    <div class="max-w-screen-xl mx-auto">
+      <h1 class="text-3xl font-semibold font-oswald text-white mt-4 mb-10">
+        Noticias sobre <span class="text-bta-pink">seguridad</span>
+      </h1>
+      <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <CardArticle
+          v-for="article in articles"
+          :key="article.id"
+          :article="article"
+        />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import CardArticle from "~/components/CardArticle.vue";
+
+export default {
+  name: "ArticlesPage",
+  components: { CardArticle },
+
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get("/api/v1/articles");
+    return { articles: data.data };
+  },
+
+
+  head() {
+    return {
+      title: "Artículos",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Preguntas y respuestas",
+        },
+      ],
+    };
+  },
+
+}
+</script>
