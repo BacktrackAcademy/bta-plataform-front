1756bff965a1d57d2ad1f0248ccea40a353c5c69
Cristopher Guzmán <guzmncristopher@gmail.com>
2023-03-01 11:49:48 -0500

    ADD: loading component

---
+++ components/icons/Spinner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="spinner animate-spin" :class="customClass"></div>
+</template>
+
+<script>
+export default {
+  name: 'SpinnerIcon',
+  props: {
+    customClass: {
+      type: String,
+      default: 'text-bg-cyma-purple',
+    },
+  },
+}
+</script>
+<style scoped>
+.spinner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  height: 36px;
+  width: 36px;
+  border-radius: 50%;
+  border-left-color: currentColor;
+}
+</style>
