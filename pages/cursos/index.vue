69e0e0b052296b0ff05191033c8f7389e991820a
Cristopher Guzmán <guzmncristopher@gmail.com>
2023-03-01 16:31:24 -0500

    FIX: loading container

--- pages/cursos/index.vue
+++ pages/cursos/index.vue
@@ -202,7 +202,7 @@
                 </div>
               </div>
             </template>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pb-10">
+            <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pb-10">
 
               <div v-for="(course, i) in courses" :key="i">
                 <div
