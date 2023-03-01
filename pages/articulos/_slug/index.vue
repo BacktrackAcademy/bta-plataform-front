0a331a0480d36917b19181bde6bb8bb0bc0c7dd3
Cristopher Guzmán <guzmncristopher@gmail.com>
2023-03-01 09:07:49 -0500

    FIX: folder slug article

--- pages/articulos/_slug/index.vue
+++ pages/articulos/_slug/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <section class="bg-bta-dark-blue">
-    <div class="w-[800px] mx-auto">
+  <section class="bg-bta-dark-blue px-4 sm:px-6 lg:px-8">
+    <div class="max-w-[800px] mx-auto">
       <article>
-        <header>
+        <header class="mb-4">
           <img :src="article.image_thumb" alt="article" />
           <h1 class="mt-5 text-white font-semibold font-oswald text-3xl">
             {{ article.title }}
           </h1>
         </header>
+        <div class="text-white font-inconsolata">
+          {{ article.short }}
+        </div>
       </article>
     </div>
   </section>
@@ -24,8 +27,9 @@ export default {
   },
   async asyncData({ params, $axios }) {
     const { data } = await $axios.get(`/api/v1/article/${params.slug}`);
-    return { article: data.data };
+    console.log(data);
+    console.log(params);
+    return { article: data };
   },
 }
-
 </script>
