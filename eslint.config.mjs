507fcb13cc8cfa23988248051cd1bad2c3e8724c
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-01-20 17:58:26 -0500

    FIX: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  antfu({
+    vue: true,
+    typescript: true,
+
+    stylistic: {
+      semi: false,
+      quotes: 'single',
+    },
+  }),
+)
