200529e3957bac2f30236e46f538fee088cda83a
Felipe Barrios <fbarrios.ing.informatico@gmail.com>
2025-01-27 16:43:08 -0300

    REFACT: auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,8 @@
-export default {
-  middleware: "auth"
-}
+// middleware/auth.js
+export default defineNuxtRouteMiddleware((to, from) => {
+  const token = useCookie('auth_token').value
+  console.log('Token:', token)
+  if (!token) {
+    return navigateTo('/login') // Redirige a login si no hay token
+  }
+})
