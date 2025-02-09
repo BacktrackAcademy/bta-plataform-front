574e1de52cd57f3d310503b659dc118ccec78a85
Felipe Barrios <fbarrios.ing.informatico@gmail.com>
2025-02-09 14:46:43 -0300

    FIX: api base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
+      apiBaseUrl: process.env.NUXT_AUTH_ORIGIN,
       authCookieName: process.env.NUXT_PUBLIC_AUTH_COOKIE_NAME,
       authTokenName: process.env.NUXT_PUBLIC_AUTH_TOKEN_NAME,
       // web3forms_access_key: '',
