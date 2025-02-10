5fe0bfb1a293db6ab329831889d8cded42591d62
Gonzalo <gologdlr@gmail.com>
2025-02-10 01:46:53 -0300

    ADD: eye

--- components/auth/UserAuthForm.vue
+++ components/auth/UserAuthForm.vue
@@ -4,10 +4,11 @@ interface Props {
 }
 
 defineProps<Props>()
-
 const emit = defineEmits<{
   register: [userInfo: { email: string, password: string }]
 }>()
+
+const showPassword = ref(false)
 
 const userInfo = ref({
   email: '',
@@ -47,15 +48,28 @@ function handleSubmit() {
           Contraseña
         </label>
         <div class="flex items-center border-2 py-2 px-3 rounded-2xl w-full">
-          <Icon name="lucide:lock" class="h-5 w-5 text-gray-400" />
-          <input
-            id="password"
-            v-model="userInfo.password"
-            class="pl-2 outline-none border-none w-full"
-            type="password"
-            placeholder="Ingresar contraseña"
-            required
-          >
+          <div class="relative w-full items-center">
+            <div class="flex items-center">
+              <Icon name="lucide:lock" class="h-5 w-5 text-gray-400" />
+              <input
+                id="password"
+                v-model="userInfo.password"
+                class="pl-2 outline-none border-none w-full"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Ingresar contraseña"
+                required
+              >
+            </div>
+            <span
+              class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
+              @click="showPassword = !showPassword"
+            >
+              <Icon
+                :name="showPassword ? 'ph:eye-bold' : 'ph:eye-closed-bold'"
+                class="size-6 text-muted-foreground"
+              />
+            </span>
+          </div>
         </div>
       </div>
 
