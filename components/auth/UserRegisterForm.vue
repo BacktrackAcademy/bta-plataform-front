9e9fe57b1b4295494d22fc523fa5e476cc5021d6
Gonzalo <gologdlr@gmail.com>
2025-02-10 01:53:00 -0300

    ADD: register

--- components/auth/UserRegisterForm.vue
+++ components/auth/UserRegisterForm.vue
@@ -17,6 +17,8 @@ const emit = defineEmits<{
 }>()
 
 const error = ref(false)
+const showPassword = ref(false)
+const showPasswordConfirmation = ref(false)
 const userInfo = ref<UserInfo>({
   email: '',
   password: '',
@@ -61,15 +63,28 @@ async function handleSubmit() {
           Contraseña
         </label>
         <div class="flex items-center border-2 py-2 px-3 rounded-2xl w-full">
-          <Icon name="lucide:lock" class="h-5 w-5 text-gray-400" />
-          <input
-            id="password"
-            v-model="userInfo.password"
-            class="pl-2 outline-none border-none w-full"
-            type="password"
-            placeholder="Ingresar contraseña"
-            required
-          >
+          <div class="relative w-full items-center">
+            <div class="flex items-center">
+              <Icon name="lucide:lock" class="h-5 w-5 text-gray-400" />
+              <input
+                id="password"
+                v-model="userInfo.password"
+                class="pl-2 outline-none border-none w-full"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Ingresar contraseña"
+                required
+              >
+            </div>
+            <span
+              class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
+              @click="showPassword = !showPassword"
+            >
+              <Icon
+                :name="showPassword ? 'ph:eye-bold' : 'ph:eye-closed-bold'"
+                class="size-6"
+              />
+            </span>
+          </div>
         </div>
       </div>
 
@@ -78,15 +93,36 @@ async function handleSubmit() {
           Reingresar contraseña
         </label>
         <div class="flex items-center border-2 py-2 px-3 rounded-2xl w-full">
-          <Icon name="lucide:lock" class="h-5 w-5 text-gray-400" />
-          <input
-            id="password_confirmation"
-            v-model="userInfo.password_confirmation"
-            class="pl-2 outline-none border-none w-full"
-            type="password"
-            placeholder="Reingresar contraseña"
-            required
-          >
+          <div class="relative w-full items-center">
+            <div class="flex items-center">
+              <Icon name="lucide:lock" class="h-5 w-5 text-gray-400" />
+              <input
+                id="password_confirmation"
+                v-model="userInfo.password_confirmation"
+                class="pl-2 outline-none border-none w-full"
+                :type="showPasswordConfirmation ? 'text' : 'password'"
+                placeholder="Reingresar contraseña"
+                required
+              >
+            </div>
+            <span
+              class="absolute end-0 inset-y-0 flex items-center justify-center px-2"
+              @click="showPasswordConfirmation = !showPasswordConfirmation"
+            >
+              <Icon
+                :name="showPasswordConfirmation ? 'ph:eye-bold' : 'ph:eye-closed-bold'"
+                class="size-6"
+              />
+            </span>
+          </div>
+        </div>
+        <div class="mt-2">
+          <p class="px-8 text-center text-xs">
+            ¿Ya tienes una Cuenta?
+            <RouterLink to="/login" class="hover:underline underline-offset-4 hover:text-primary">
+              Conectarse
+            </RouterLink>
+          </p>
         </div>
       </div>
 
