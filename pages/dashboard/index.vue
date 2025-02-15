2707aaea5ab321032864a15e1cf7686cb4972c05
Felipe Barrios <fbarrios.ing.informatico@gmail.com>
2025-02-15 14:26:40 -0300

    FIX: css

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -130,7 +130,7 @@ useSeoMeta({
         <h2 class="text-white text-lg font-oswald tracking-normal my-4">
           Descubre nuestras especialidades
         </h2>
-        <div class="flex items-center gap-y-4 gap-4 rounded-md overflow-x-auto flex-nowrap pb-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full h-full">
           <div
             v-for="(degree, i) in degrees"
             :key="i"
