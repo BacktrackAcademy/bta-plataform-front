6834167ad52e29d7bc0f28c0966a0aca50b01f8d
Felipe Barrios <fbarrios.ing.informatico@gmail.com>
2025-02-14 23:10:53 -0300

    ADD: link

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -37,7 +37,7 @@
 
         <li class="px-4 lg:w-full">
           <NuxtLink
-            to="/dashboard"
+            to="/mi-progreso"
             class="flex items-center font-medium gap-5 pl-3 py-3 pr-1 text-[#c4c8ce] hover:text-white transition-colors duration-200 hover:bg-[#1e2229] group rounded-sm"
           >
             <Icon name="radix-icons:bar-chart" class="size-6" />
