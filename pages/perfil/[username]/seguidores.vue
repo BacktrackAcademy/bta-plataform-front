ba2482cd2c695ca73f9272c7c4bd95313896fc0d
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-02-19 15:36:44 -0500

    REFACT: code

--- pages/perfil/[username]/seguidores.vue
+++ pages/perfil/[username]/seguidores.vue
@@ -1,24 +1,31 @@
 <script lang="ts" setup>
-const route = useRoute('perfil-username')
-const { username } = route.params
 const config = useRuntimeConfig()
+const { username } = useRoute('perfil-username').params
 
 interface Follower {
   id: number
   name: string
+  lastname: string
   username: string
   avatar: string
 }
 
-const { data: followers } = useFetch<Follower[]>(`${config.public.apiBaseUrl}/profile/${username}/followers`)
+const { data: followers } = useFetch<Follower[]>(`${config.public.apiBaseUrl}/profile/${username}/followers`, {
+  lazy: true,
+})
 </script>
 
 <template>
-  <div class="space-y-4">
-    <h2 class="text-2xl text-white font-bold font-oswald mb-6">
+  <div class="space-y-4 pb-16">
+    <h2 class="text-2xl font-bold font-oswald mb-6">
       Seguidores
     </h2>
-    <div class="space-y-4">
+    <div v-if="followers?.length === 0">
+      <p class="text-muted-foreground">
+        No tienes seguidores
+      </p>
+    </div>
+    <div v-else class="space-y-4 w-full md:w-3/4">
       <FollowerCard
         v-for="follower in followers"
         :key="follower?.id"
